d63a88dd9224df6777a2104aecced261ee948b27
romansosa <romansf15@gmail.com>
2026-06-12 22:54:14 -0600

    Add Classwork 08 - Numerical Integration fixed

--- CW08/cw08.py
+++ CW08/cw08.py
@@ -10,10 +10,10 @@ b = input( "Write the right endopoint of the interval: ")
 if "pi" in b:
     b = eval(b.replace("pi", str(math.pi)))
 else:
-    b = float(a)
+    b = float(b)
     
 f_x = input("Write the function to integrate")
-method = input("Select integration method (LRM/RRM/MPM)")
+method = input("Select integration method (LRM/RRM/MPM/TRP)")
 
 #PROCESS
 area = 0.0
@@ -21,35 +21,32 @@ n = 1000
 h = (b - a) / n
 shift = 0
 constant = 0
-variable = 0
-
-if method == "TRAP":
-    variable = 1
-    
-    f_0 = f_x.replace("x", f"({a})")
-    area += (h/2) * eval(f_0)
-    
-    for i in range(variable, n):
-        xi = a + (i * h)
-        f_xi = f_x.replace("x", f"({xi})")
-        area += (h/2) * 2 * eval(f_xi)
-        
-    f_xn = f_x.replace("x", f"({b})")
-    area += (h/2) * eval(f_xn) 
 
 if method == "RRM":
     shift = 1
     
 if method == "MPM":
     constant = h/2
-else:
-    pass
+
+if method == "TRP":
+    shift = 1
 
 for i in range(0 + shift, n + shift):
     xi = a + i * h + constant
     
-    height = eval(f_x.replace("x", str(xi)))
-    area += height * h
+    x = xi
+    height = eval(f_x)
+    if method == "TRP":
+        area += 2 * height * h
+    else:
+        area += height * h
+        
+if method == "TRP":
+    x = a
+    fa = eval(f_x)
+    x = b
+    fb = eval(f_x)
+    area = (area + (fa * h) + (fb * h)) / 2
     
 #OUTPUT
-print(f" The integration of {f_x} is {area:.4f}") 
+print(f" The integration of {f_x} is {area:.4f}")
