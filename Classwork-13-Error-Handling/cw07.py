8c0cdfcd469cc5bf5436e5aa61f32f320140af87
romansosa <romansf15@gmail.com>
2026-07-10 09:32:24 -0600

    Classwork 13, cw07 correción con logs

--- Classwork-13-Error-Handling/cw07.py
+++ Classwork-13-Error-Handling/cw07.py
@@ -1,34 +1,43 @@
 class DigitoApocrifo(Exception):
     pass
 
-check = True
 
-while check:
-    try:
-        rol = input("Escribe el rol: ")
-        rol_sin_digito, digito = rol.split("-")
-        check = False 
+rol = input("Escribe el rol: ")
     
-    except ValueError:
-        print("Rol no válido, debe tener 11 dígitos y el último separado por un guión")
 
-inverso = rol_sin_digito[::-1]
+partes = rol.split("-")
+if len(partes) != 2:
+    print("No tiene el formato XXXXXXXXX-X")
+else:
+    rol_sin_digito, digito = partes
+        
+   
+    if not rol_sin_digito.isdigit():
+        print("Los digitos del rol deben ser numéricos")
+    elif not digito.isdigit():
+        print("El digito verificador debe ser numérico")
+    else:
+        
+        inverso = rol_sin_digito[::-1]
+        secuencia = [2, 3, 4, 5, 6, 7]
+        suma = 0
 
-secuencia = [2,3,4,5,6,7]
-suma = 0
+        for index in range(len(inverso)):
+            numero = int(inverso[index])
+            suma += numero * secuencia[index % 6]
 
-for index in range(len(inverso)):
-    numero = int(inverso[index])
-    suma += numero * secuencia [index % 6]
+        resultado = suma % 11
+        verificador = 11 - resultado
 
-resultado = suma % 11
-
-verificador = 11 - resultado
-
-try: 
-    if verificador != digito:
-        raise DigitoApocrifoError
-except DigitoApocrifoError as e:
-        print("Digito verificador apocrifo")
-    
-print(f"{rol_sin_digito}-{verificador}")
+        
+        try: 
+            if int(digito) != verificador:
+                
+                raise DigitoApocrifo(f"El dígito verificador no conicide, se esperaba {verificador}")
+            
+           
+            print(f"{rol_sin_digito}-{verificador}")
+            
+        except DigitoApocrifo as e:
+            
+            print(e)
